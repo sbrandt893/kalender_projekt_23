e02d61012343980768c3ddfc33eb404a0f8f4c69
Stefan Brandt <s.brandt893@gmail.com>
2023-08-02 08:53:09 +0200

    date infos etc dynamic

---
+++ database.js
@@ -0,0 +1,14 @@
+
+// window.onload = function () { main(); };
+
+// function main() {
+
+
+//     // Map with all months and an description
+//     const monthInformations = new Map([
+//         ["Januar", "Der Januar ist der erste Monat des Jahres im gregorianischen und im julianischen Kalender. Er hat 31 Tage. Veraltete Namensformen sind Hartung, Hartmonat, Schneemonat, Eismond, Wintermonat oder Wolfsmonat."],
+//         ["Februar", "Der Februar ist der zweite Monat des Jahres im gregorianischen Kalender. Er hat 28 Tage, in Schaltjahren 29 Tage. Der Name leitet sich vom lateinischen Wort februare für „reinigen“ ab."],
+//         ["März", "Der März ist der dritte Monat des Jahres im gregorianischen Kalender. Er hat 31 Tage. Der Name leitet sich vom römischen Kriegsgott Mars ab."],
+//     ]);
+
+// }
